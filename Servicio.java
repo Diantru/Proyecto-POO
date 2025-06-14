2b7cf30af125dccffc57c62865800218802b53c0
Diantru <geandesi@espol.edu.ec>
2025-06-14 16:42:05 -0500

    Update Servicio.java

--- Servicio.java
+++ Servicio.java
@@ -1,1 +1,24 @@
+pubic class Servicio{
+  private int codigo;
+  private String nombre;
+  private double precio;
 
+  public Servicio(int code, String n, double precio){
+    this.codigo = code;
+    this.nombre = n;
+    this.precio = precio;
+  }
+
+  public int getCodigo(){
+    return codigo;
+  }
+
+  public String getNombre(){
+    return nombre;
+  }
+
+  public double getPrecio(){
+    return precio;
+  }
+    
+}
