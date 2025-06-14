cb81394ff58bc4cf7d865914b5aab4e491954754
Diantru <geandesi@espol.edu.ec>
2025-06-14 14:17:15 -0500

    Update Factura.java

--- Factura.java
+++ Factura.java
@@ -1,6 +1,15 @@
 public class Factura{
-  Cliente cliente;
-  String mes;
-  String anio;
+  private Cliente cliente;
+  private String mes;
+  private String anio;
+  private List<Ordenes> ordenes;
+
+  public Factura(Cliente c, String mes, String anio, Ordenes orden){
+    this.cliente = c;
+    this.mes = mes;
+    this.anio = anio;
+    
+  }
   
+  public 
 }
