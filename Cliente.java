c94b010b8536d08f8feca37db8e4fb6188022966
Valeria Noriega <109978901+noriegitus@users.noreply.github.com>
2025-06-14 13:44:18 -0500

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,4 +1,11 @@
 
-class Cliente extends Parent {
-  
+class Cliente extends Persona {
+  String direccion;
+  TipoCliente tipoCliente;
+
+  public Cliente(String  id, String nombre, int telefono, String direccion, tipoCliente tipoCliente){
+    super(id, nombre, telefono);
+    this.direccion = direccion;
+    this.tipoCliente = tipoCliente;
+  }
 }
