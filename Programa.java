730e17c6a746bdffcfe7d7d007092f6d761e31f0
Diantru <geandesi@espol.edu.ec>
2025-06-14 16:43:51 -0500

    Update Programa.java

--- Programa.java
+++ Programa.java
@@ -7,6 +7,18 @@ public class Programa{
     ArrayList<Proveedor> proveedores = new ArrayList<>;
     ArrayList<Servicio> servicios = new ArrayList<>;
   }
+
+  public void mostrarMenu(){
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    System.out.println();
   
   public static void main(String[] args){
     Programa.inicializarApp();
