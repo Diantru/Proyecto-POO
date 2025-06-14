12e674cf2b6f3d520e61482f4d6bc26489b2785a
Emilio Jaramillo <emijaram@espol.edu.ec>
2025-06-14 13:58:32 -0500

    Update Tecnicos.java

--- Tecnicos.java
+++ Tecnicos.java
@@ -1,1 +1,29 @@
+import java.util.ArrayList;
+
+public class Tecnico extends Persona {
+    private Especialidades especialidad;
+    private ArrayList<Orden> ordenes;
+
+    public Tecnico(String id, String nombre, int telefono, Especialidades especialidad) {
+        super(id, nombre, telefono);
+        this.especialidad = especialidad;
+        this.ordenes = new ArrayList<>();
+    }
+
+    public void registrarOrden(Orden nuevaOrden) {
+        ordenes.add(nuevaOrden);
+    }
+
+    public void asignarOrden(Orden orden) {
+        ordenes.add(orden);
+    }
+
+    public Especialidades getEspecialidad() {
+        return especialidad;
+    }
+  
+    public ArrayList<Orden> getOrdenes() {
+        return ordenes;
+    }
+}
 
