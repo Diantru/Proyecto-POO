3b105a636ae6b5a5016e8e17124b276127cd9d57
Valeria Noriega <109978901+noriegitus@users.noreply.github.com>
2025-06-14 14:20:16 -0500

    Update Ordenes.java

--- Ordenes.java
+++ Ordenes.java
@@ -1,1 +1,15 @@
 
+class Orden extends Cliente {
+  private String id;
+  private String fecha;
+  private tipoVehiculo tiposVehiculo;
+  private String placaCarro;
+  private int valorPagar;
+
+  public Orden(String id, String fecha, tiposVehiculos tipoVehiculo, Servicios[] serviciosUsar){
+    this.id = id;
+    this.fecha = fecha;
+    this.tipoVehiculo = tiposVehiculo;
+    this.serviciosUsar = Servicios[]
+  }
+}
