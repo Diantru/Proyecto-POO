b4dbbcb8d15c206a7e49012cb4eb81cd90e73e9c
Diantru <geandesi@espol.edu.ec>
2025-06-14 13:52:19 -0500

    Delete Proveedores.java

--- Proveedores.java
+++
@@ -1,9 +0,0 @@
-public class Proveedor extends Persona{
-    private descripcion;
-
-    public Proveedor(String id, String n, int tel, String desc){
-        super(id, n, tel);
-        String 
-        
-    }
-}
