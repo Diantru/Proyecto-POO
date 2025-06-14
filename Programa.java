b4a3ecb60bbb5442bc1f3cce6b550118654ec04d
Diantru <geandesi@espol.edu.ec>
2025-06-14 16:38:07 -0500

    Update Programa.java

--- Programa.java
+++ Programa.java
@@ -5,6 +5,7 @@ public class Programa{
     ArrayList<Cliente> clientes = new ArraryList<>;
     ArrayList<Ordenes> ordenes = new ArrayList<>;
     ArrayList<Proveedor> proveedores = new ArrayList<>;
+    ArrayList<String> servicios = new ArrayList<>;
   }
   
   public static void main(String[] args){
