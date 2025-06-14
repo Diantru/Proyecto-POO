6a1bd3ffdafe8db93e912e91ad1f30c4b0371a76
Diantru <geandesi@espol.edu.ec>
2025-06-14 13:39:19 -0500

    Update Persona.java

--- Persona.java
+++ Persona.java
@@ -2,4 +2,7 @@ public class Persona{
   private String identificacion;
   private String nombre;
   private int telefono;
+
+  public Persona(String id, String, int tel){
+    this.identificacion = id
 }
